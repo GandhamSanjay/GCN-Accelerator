ea5dcc1c8e28af0c9edd2d20f78dc18b1b795ac8
GandhamSanjay <sanjaygandham@knights.ucf.edu>
2022-10-21 18:48:33 -0400

    SpMM instruction added

--- test/inst.py
+++ test/inst.py
@@ -22,6 +22,16 @@ class inst:
         logging.debug(f"inst[127:0] = {inst}")
         return inst
 
+    def spMM(self,  dep = '0000', sram_offset_col = 0, sram_offset_ptr = 0, sram_offset_den = 0, xsize = 0, ysize = 0):
+        op = '10'
+        logging.debug(f"Creating load with \ndep = {dep}\nid = {id}\nsram_offset_col = {sram_offset_col}\nsram_offset_ptr = {sram_offset_ptr}\
+        \nsram_offset_den = {sram_offset_den}\nxsize = {xsize}\nysize = {ysize}")
+        inst = op[::-1] + dep[::-1] + binary_repr(sram_offset_col, 16)[::-1] + binary_repr(sram_offset_ptr, 16)[::-1] + binary_repr(sram_offset_den, 16)[::-1]
+        inst =  inst + binary_repr(xsize, 7)[::-1] + binary_repr(ysize, 7)[::-1]
+        inst = inst + '0'*(128-len(inst))
+        logging.debug(f"inst[127:0] = {inst}")
+        return inst
+
     def randLoad(self, count = 1):
         instSeq = ""
         for num in range(count):
@@ -30,7 +40,7 @@ class inst:
                 instSeq = instSeq + str(random.randint(0,1))
         return instSeq
     
-    def randSpmm(self, count = 1):
+    def randSpMM(self, count = 1):
         instSeq = ""
         for num in range(count):
             instSeq = instSeq + "010000000"
@@ -42,9 +52,9 @@ class inst:
      
 f = open('ram.txt','w')
 instGen = inst()
-f.write(instGen.load(xsize = 0))
-f.write(instGen.randSpmm())
-f.write(instGen.load(xsize = 0))
-f.write(instGen.randSpmm())
+f.write(instGen.load(xsize = 16))
+f.write(instGen.spMM(xsize = 16))
+# f.write(instGen.load(xsize = 0))
+# f.write(instGen.spMM())
 # for I in range(4):
 #     f.write(instGen.load(xsize = 64))
