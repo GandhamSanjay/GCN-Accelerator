dbb77499374bb1a26a22c035f5ef532566d9b1d0
Sanjay Gandham <sanjaygandham@knights.ucf.edu>
2022-10-26 00:11:55 -0400

    PE refactoring (Incomplete)

--- hardware/src/main/scala/core/PE.scala
+++ hardware/src/main/scala/core/PE.scala
@@ -31,8 +31,6 @@ class PECSR(debug: Boolean = false)(implicit p: Parameters) extends Module with 
   })
 
   val writeEnVec = Vec(cp.nScratchPadMem, Wire(Bool()))
-  val readCmdVec = Vec(cp.nScratchPadMem, new SPReadCmd)
-  val readDataVec = Vec(cp.nScratchPadMem, new SPReadData)
 
   for(i <- 0 until cp.nScratchPadMem){
     io.spWrite(i).ready := true.B
@@ -45,81 +43,88 @@ class PECSR(debug: Boolean = false)(implicit p: Parameters) extends Module with 
   val spPtr = Module(new Scratchpad(scratchType = "Ptr"))
   val spDen = Module(new Scratchpad(scratchType = "Den"))
   io.spWrite(0)  <> spVal.io.spWrite
-  readCmdVec(0)  <> spVal.io.spReadCmd
-  readDataVec(0) <> spVal.io.spReadData
   writeEnVec(0)  <> spVal.io.writeEn
   io.spWrite(1)  <> spDen.io.spWrite
-  readCmdVec(1)  <> spDen.io.spReadCmd
-  readDataVec(1) <> spDen.io.spReadData
   writeEnVec(1)  <> spDen.io.writeEn
   io.spWrite(2)  <> spPtr.io.spWrite
-  readCmdVec(2)  <> spPtr.io.spReadCmd
-  readDataVec(2) <> spPtr.io.spReadData
   writeEnVec(2)  <> spPtr.io.writeEn
   io.spWrite(3)  <> spCol.io.spWrite
-  readCmdVec(3)  <> spCol.io.spReadCmd
-  readDataVec(3) <> spCol.io.spReadData
   writeEnVec(3)  <> spCol.io.writeEn
 
   val blockSizeBytes = (cp.blockSize/8)
 
   // Registers 
-  val sSel = RegInit(0.U(cp.nScratchPadMem.W))
-  val sAddr = RegInit(0.U(C_SRAM_OFFSET_BITS.W))
-  val sValid = Reg(Bool())
-  val ptrStartIdx = RegInit(0.U(32.W))
-  val ptrEndIdx = RegInit(0.U(32.W))
   val ptrCurr = RegInit(0.U(32.W))
-  val valCurr = RegInit(0.U(cp.blockSize.W))
-  val colCurr = RegInit(0.U(32.W))
+  val colCurr_q = RegInit(0.U(32.W))
+
   val macCount = RegInit(0.U(32.W)) 
   val acc = RegInit(0.U(cp.blockSize.W))
-  val denCol = RegInit(0.U(32.W))
 
   // state machine
-  val sIdle :: sRowPtr1 :: sRowPtr2 :: sCol :: sMAC:: sOut :: Nil = Enum(8)
+  val sIdle :: sRowPtr1 :: sRowPtr2 :: sCol :: sValDen :: sMAC:: sOut :: Nil = Enum(8)
   val state = RegInit(sIdle)
   val stateNext = RegInit(sIdle)
-  io.peReq.ready := (state === sIdle)
+  io.peReq.ready := (state === sIdle) || done
   val start = io.peReq.fire
   val peReq_q = RegEnable(io.peReq.bits, start)
 
   val rowPtrAddr = io.peReq.bits.sramPtr + ((io.peReq.bits.rowIdx) << log2Ceil(cp.blockSize/8))
   val rowPtrAddr_q = peReq_q.sramPtr + ((peReq_q.rowIdx) << log2Ceil(cp.blockSize/8))
-
   val rowPtrSPAddr = Mux(start,rowPtrAddr, rowPtrAddr_q)
   val rowPtrSPAddrNext = rowPtrSPAddr + 1.U
+  spPtr.io.spReadCmd.addr := Mux((state === sIdle), rowPtrSPAddr, rowPtrSPAddrNext)
+
+  val colIdxAddr = peReq_q.sramColVal + (ptrCurr << log2Ceil(cp.blockSize/8))
+  spCol.io.spReadCmd.addr := colIdxAddr
+  val colCurr = Mux(state === sCol, spCol.io.spReadData.data, colCurr_q)
+
+  val valAddr = io.peReq.bits.sramColVal + (ptrCurr << log2Ceil(cp.blockSize/8))
+  spVal.io.spReadCmd.addr := valAddr
+  val denAddr = io.peReq.bits.sramDen + ((colCurr << log2Ceil(cp.blockSize/8)) << Log2(peReq_q.denXSize)) + (peReq_q.rowIdx << log2Ceil(cp.blockSize/8))
+  spDen.io.spReadCmd.addr := denAddr
+
   val rowPtr1Data = Reg(UInt(32.W))
   val rowPtr2Data = Reg(UInt(32.W))
   val nonZeroInRow = Mux((state === sRowPtr2), spPtr.io.spReadData.data - rowPtr1Data, rowPtr2Data - rowPtr1Data)
+  val done = ((state === sRowPtr2) && (nonZeroInRow === 0.U)
+             || ((state === sMAC) && (ptrCurr === rowPtr2Data)))
 
-  val SPValid = Wire(Bool())
-  val SPAddr = Wire(chiselTypeOf((new SPReadCmd).addr))
-  SPValid := ((state === sIdle) && start)
-  SPAddr := (state === sIdle) && start
 
   switch(state){
     is(sIdle){
       when(start){
+        acc := 0.U
         state := sRowPtr1
       }
     }
     is(sRowPtr1){
       state := sRowPtr2
       rowPtr1Data := spPtr.io.spReadData.data
+      ptrCurr := spPtr.io.spReadData.data
     }
     is(sRowPtr2){
-      state := sCol
       rowPtr2Data := spPtr.io.spReadData.data
+      when(nonZeroInRow === 0.U){
+        state := sIdle
+      }.otherwise{
+        state := sCol
+      }
     }
     is(sCol){
       state := sMAC
+      colCurr := spCol.io.spReadData.data
+      state := sValDen
+    }
+    is(sValDen){
+      ptrCurr := ptrCurr + 1.U
     }
     is(sMAC){
-      state := sOut
-    }
-    is(sOut){
-      state := sIdle
+      acc :=  acc + (spVal.io.spReadData.data * spDen.io.spReadData.data)
+      when(ptrCurr === rowPtr2Data){
+        state := sIdle
+      }.otherwise{
+        state := sCol
+      }
     }
   }
 }
