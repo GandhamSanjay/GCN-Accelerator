76ef02c9b4f74331cd30ee4b55918ff6fd3c7116
GandhamSanjay <sanjaygandham@knights.ucf.edu>
2022-10-27 20:03:21 -0400

    PE refactoring, row mapping to pe fixed (Incomplete)

--- hardware/src/main/scala/core/Compute.scala
+++ hardware/src/main/scala/core/Compute.scala
@@ -27,29 +27,13 @@ class Compute(debug: Boolean = false)(implicit p: Parameters) extends Module wit
     Module(new PECSR)
   } 
 
-  // var peIsFree = (peArray(0).io.peReq.ready && !peArray(0).io.peReq.valid)
-  var peIsFree = (peArray(0).io.peReq.ready)
-  var peFree = peIsFree
-  var peAllFree = peIsFree
-  var peFreeCount = peIsFree
 
-  // PE stuff for more than 1 in number
-  if(cp.nPE > 1){
-     peIsFree := (for (i <- 0 until cp.nPE) yield {
-    (peArray(i).io.peReq.ready)
-    }).reduce(_||_)
-     peFree := (for (i <- 0 until cp.nPE) yield {
-      (peArray(i).io.peReq.ready).asUInt
-    }).reduce(Cat(_,_))
-     peAllFree := (for (i <- 0 until cp.nPE) yield {
-      peArray(i).io.peReq.ready
-    }).reduce(_&&_)
-     peFreeCount := PopCount(peFree)
-  }
- 
+  val peAllFree = if(cp.nPE>1){(for (i <- 0 until cp.nPE) yield {
+    peArray(i).io.peReq.ready
+  }).reduce(_&&_)}else{ peArray(0).io.peReq.ready}
 
   // state machine
-  val sIdle :: sAssign :: sWait :: sDone :: Nil = Enum(4)
+  val sIdle :: sWait :: sDone :: Nil = Enum(3)
   val state = RegInit(sIdle)
   val start = inst_q.io.deq.fire
   val ctr = RegInit(0.U(5.W))
@@ -57,9 +41,13 @@ class Compute(debug: Boolean = false)(implicit p: Parameters) extends Module wit
   val inst = RegEnable(inst_q.io.deq.bits, start)
   val blockSizeBytes = (cp.blockSize/8)
   val currRow = RegInit(0.U(32.W))
-  val assignStart = (((state === sIdle) && start)
-                    || (state === sAssign))
-  val rowRem = dec.io.ySizeSp - currRow
+
+  for(i <- 0 until cp.nPE){
+    peArray(i).io.peNum := i.U
+    peArray(i).io.peReq.valid := start
+    peArray(i).io.peReq.bits.spYSize := dec.io.ySizeSp
+  }
+
 
   assert (dec.io.ySizeSp =/= 9.U)
 
@@ -67,31 +55,6 @@ class Compute(debug: Boolean = false)(implicit p: Parameters) extends Module wit
 // Assigns the lowest row index to the first available PE , 2nd lowest row index to next 
 // available PE and so on during the same clock cycle. Sets the valid high according to
 // number of higher priority PE that are free and number of rows remaining to be assigned
-  
-  
-  val rowAssignment  = WireDefault(VecInit(Seq.tabulate(cp.nPE)(n => (currRow + n.U))))
-  if(cp.nPE == 1){
-    peArray(0).io.peReq.bits.rowIdx := currRow
-    peArray(0).io.peReq.valid := assignStart && (rowRem > 0.U) 
-  }else{
-    for (i <- 0 until cp.nPE){
-      if(i == 0){ 
-        peArray(i).io.peReq.bits.rowIdx := rowAssignment(0)
-        peArray(i).io.peReq.valid := assignStart && (rowRem > 0.U) 
-      }else{
-        val vectorRowAssignment = VecInit(Seq.tabulate(i)(n => rowAssignment(n)))
-        if(i == 0){
-          val otherPEReady = PopCount(peFree(0))
-          peArray(i).io.peReq.bits.rowIdx := MuxTree(otherPEReady, vectorRowAssignment)
-          peArray(i).io.peReq.valid := assignStart && (rowRem > otherPEReady)
-        }else{
-          val otherPEReady = PopCount(peFree(i-1, 0))
-          peArray(i).io.peReq.bits.rowIdx := MuxTree(otherPEReady, vectorRowAssignment)
-          peArray(i).io.peReq.valid := assignStart && (rowRem > otherPEReady)
-        }
-      }
-    }
-  }
 
   for(i <- 0 until cp.nPE){
     peArray(i).io.peReq.bits.denXSize := dec.io.xSizeDen
@@ -103,7 +66,7 @@ class Compute(debug: Boolean = false)(implicit p: Parameters) extends Module wit
   val done = RegInit(0.U(32.W))
   io.done := done
 
-//   // instruction queue
+  // instruction queue
   dec.io.inst := Mux(start, inst_q.io.deq.bits, inst)
 
   // control
@@ -112,20 +75,7 @@ class Compute(debug: Boolean = false)(implicit p: Parameters) extends Module wit
       done := false.B
       currRow := 0.U
       when(start){
-        currRow := currRow + cp.nPE.U
-        when((currRow + cp.nPE.U) >= dec.io.ySizeSp){
-          state := sWait
-        }.otherwise{
-          state := sAssign
-        }
-      }
-    }
-    is(sAssign){
-      currRow := currRow + peFreeCount
-      when((currRow + peFreeCount) >= dec.io.ySizeSp){
         state := sWait
-      }.otherwise{
-        state := sAssign
       }
     }
     is(sWait){
