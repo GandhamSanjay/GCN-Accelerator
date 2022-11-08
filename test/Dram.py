6258d079d6dfc15ca4806d0441f37af08f70ad9b
GandhamSanjay <sanjaygandham@knights.ucf.edu>
2022-11-08 15:53:57 -0500

    More testcases capability

--- test/Dram.py
+++ test/Dram.py
@@ -2,9 +2,9 @@ from numpy import binary_repr
 import numpy as np
 import logging
 from inst import inst
+from scipy.sparse import csr_matrix
 
-class data:
-            
+class data:       
     ### Generates data for GCN-Accelerator
     def __init__(self):
         logging.debug("Data generator created")
@@ -14,43 +14,45 @@ class data:
             for j in i:
                 string_A = string_A + binary_repr(j, 32)[::-1]
         return string_A
+
     def arrayToBinary(self, A):
         string_A = ''
         for i in A:
                 string_A = string_A + binary_repr(i, 32)[::-1]
         return string_A
 
+    def randSP(self,spDim, denDim):
+        I = np.random.randint(2, size = spDim)
+        print(f"\nSparse matrix is = \n{I}")
+        Sparse = csr_matrix(I)
+        val = Sparse.data
+        print(f"\nVal is =\n {val}")
+        col = Sparse.indices
+        print(f"\nCol_Idx  is = \n{col}")
+        row = Sparse.indptr
+        print(f"\nRow_ptr is = \n{row}")
+        W = np.random.randint(100, size = denDim)
+        print(f"\nDense matrix is = \n{W}")
+        O = np.matmul(I,W)
+        print(f"\nOutput matrix is = \n{O}")
+        return ((val,col,row), I, W, O)
 
 
-I = np.array([[0, 11, 0, 0],
-            [22, 0, 0, 33],
-            [0, 0, 0, 0],
-            [0, 44, 0, 0]])
-W = np.array([[2, 2],
-            [1, 3],
-            [3, 1],
-            [1, 2]])
-O = np.matmul(I,W)
-psum = np.ones(O.shape, dtype =int)
-val = np.array([11,22,33,44])
-col = np.array([1,0,3,1])
-row = np.array([0,1,3,3,4])
-den = np.array([[2, 2],
-            [1, 3],
-            [3, 1],
-            [1, 2]])
+np.random.seed(0)
 dataGen = data()
+((val,col,row), I, den, O) = dataGen.randSP(spDim = (8,8), denDim = (8,4))
+psum = np.zeros(O.shape, dtype =int)
 rowBin = dataGen.arrayToBinary(row)
 colBin = dataGen.arrayToBinary(col)
 valBin = dataGen.arrayToBinary(val)
 denBin = dataGen.matrixToBinary(den)
 psumBin = dataGen.matrixToBinary(psum)
-rowAddr = pow(2,8)
-colAddr = 2 * pow(2,8)
-valAddr = 3 * pow(2,8)
-denAddr = 4 * pow(2,8)
-outAddr = 6 * pow(2,8)
-psumAddr = 5 * pow(2,8)
+rowAddr = pow(2,10)
+colAddr = 2 * pow(2,10)
+valAddr = 3 * pow(2,10)
+denAddr = 4 * pow(2,10)
+psumAddr = 5 * pow(2,10)
+outAddr = 6 * pow(2,10)
 (_,x) = den.shape
 (y,_) = I.shape
 instGen = inst()
